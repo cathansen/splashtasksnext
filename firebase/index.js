6ce63dd08a62d1f320fc4529ae7afb1cf91aeeae
Catherine Hansen <cat.hansen.w@gmail.com>
2021-11-17 14:23:22 -0700

    first commit

---
+++ firebase/index.js
@@ -0,0 +1,9 @@
+// Build the app instance of Firebase
+import { initializeApp } from 'firebase/app';
+
+import firebaseConfig from "./firebaseConfig";
+
+const firebase = initializeApp(firebaseConfig);
+
+
+export default firebase
